ab46ad48e24207ef20d2c747915a2f06dd161b9c
MacBook <macbook@MacBook-Pro.local>
2025-06-20 20:47:19 +0530

    Initial project with Vue and API integration

--- src/components/ClientsList.vue
+++ src/components/ClientsList.vue
@@ -4,7 +4,8 @@
     <ul>
       <li v-for="client in clients" :key="client.id">
   {{ client.givenName }} {{ client.familyName }} – {{ client.email }}<br />
-  //<strong>Client ID:</strong> {{ client.id }}<br><br />
+  <!--<strong>Client ID:</strong> {{ client.id }}<br><br />-->
+  <!--<strong>Company ID:</strong> {{ client.companyId }}<br><br />-->
 </li>
     </ul>
   </div>
